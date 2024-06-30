a2cba2cbd2a0011a549df3b18f0f4f694e7a3e70
op4804 <op4804@gmail.com>
2024-06-30 20:30:03 +0900

    feat: boss stage basic function, docs: 28mom
    28일 회의록 추가
    보스 스테이지 진입시 카메라 고정,
    플랫폼 및 벽 생성

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -8,19 +9,34 @@ public class CameraFollow : MonoBehaviour
 
     [SerializeField] private Vector3 offset;
 
+    public bool isBossStage = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(SceneManager.GetActiveScene().name == "BossStage")
+        {
+            isBossStage = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CharacterManager.Instance)
+        if (!isBossStage)
         {
-            transform.position = Vector3.Lerp(transform.position, CharacterManager.Instance.transform.position + offset, followSpeed);
+            if (CharacterManager.Instance)
+            {
+                transform.position = Vector3.Lerp(transform.position, CharacterManager.Instance.transform.position + offset, followSpeed);
+
+            }
+        }
+        else
+        {
+            transform.position = new Vector3(0,-7.5f,-10);
+            this.GetComponent<Camera>().orthographicSize = 24;
 
         }
+
     }
 }
